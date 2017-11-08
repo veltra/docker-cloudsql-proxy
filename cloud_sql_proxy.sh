13079633026cef207f5ae6504406369d141e0dd0
KunihikoKido <kunihiko.kido@veltra.com>
2017-11-08 09:40:04 +0900

    Change Environment Variable Name

--- cloud_sql_proxy.sh
+++ cloud_sql_proxy.sh
@@ -4,6 +4,6 @@ set -eu
 echo ${GCLOUD_SERVICE_KEY} | base64 --decode --ignore-garbage > ${HOME}/gcloud-service-key.json
 
 cloud_sql_proxy \
-  -instances=${INSTANCEL_CONNECTION_NAME}=tcp:0.0.0.0:3306 \
+  -instances=${CLOUDSQL_CONNECTION_NAME}=tcp:0.0.0.0:3306 \
   -credential_file=${HOME}/gcloud-service-key.json \
   -dir=${HOME}/cloudsql
